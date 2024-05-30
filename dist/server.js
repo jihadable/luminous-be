72de5ca13b8db50b090080d4393b39945984a4b4
jihadable <umarjihad141004@gmail.com>
2024-05-30 10:08:56 +0700

    create static root endpoint

--- dist/server.js
+++ dist/server.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const userRoute_1 = __importDefault(require("./routes/userRoute"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use("/api/users", userRoute_1.default);
+app.use(express_1.default.json(), express_1.default.static("views"));
+app.use("/styles", express_1.default.static(path_1.default.join(__dirname, "styles")));
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(path_1.default.join(__dirname, "views", "index.html"));
 });
+app.use("/api/users", userRoute_1.default);
 app.listen(8000, () => {
     console.log("Server is running");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express, { Application, Request, Response } from 'express'
+import path from 'path'
 import userRouter from './routes/userRoute'
 
 const app: Application = express()
 
-app.use(express.json())
-
-app.use("/api/users", userRouter)
+app.use(express.json(), express.static("views"))
+app.use("/styles", express.static(path.join(__dirname, "styles")))
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!")
+    res.sendFile(path.join(__dirname, "views", "index.html"))
 })
+
+app.use("/api/users", userRouter)
 
 app.listen(8000, () => {
     console.log("Server is running")
