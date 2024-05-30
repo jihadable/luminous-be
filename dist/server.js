adc1e5ed18f812495faaff6bc4e41651e70078ec
jihadable <umarjihad141004@gmail.com>
2024-05-30 10:20:41 +0700

    modify server.ts

--- dist/server.js
+++ dist/server.js
@@ -1,43 +1,19 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importStar(require("express"));
+const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const userRoute_1 = __importDefault(require("./routes/userRoute"));
 const app = (0, express_1.default)();
-const router = (0, express_1.Router)();
 app.use(express_1.default.json(), express_1.default.static("views"));
 app.use("/styles", express_1.default.static(path_1.default.join(__dirname, "styles")));
-router.get("/", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "views", "index.html"));
+app.get("/", (req, res) => {
+    res.send("RUN");
+    // res.sendFile(path.join(__dirname, "views", "index.html"))
 });
 app.use("/api/users", userRoute_1.default);
-app.use("/", router);
 app.listen(8000, () => {
     console.log("Server is running");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
-import express, { Application, Request, Response, Router } from 'express'
+import express, { Application, Request, Response } from 'express'
 import path from 'path'
 import userRouter from './routes/userRoute'
 
 const app: Application = express()
-const router: Router = Router()
 
 app.use(express.json(), express.static("views"))
 app.use("/styles", express.static(path.join(__dirname, "styles")))
 
-router.get("/", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"))
+app.get("/", (req: Request, res: Response) => {
+    res.send("RUN")
+    // res.sendFile(path.join(__dirname, "views", "index.html"))
 })
 
 app.use("/api/users", userRouter)
-
-app.use("/", router)
 
 app.listen(8000, () => {
     console.log("Server is running")
