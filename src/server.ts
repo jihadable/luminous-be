4003a5c326ebd850696aa6e677cc75ba777598d6
jihadable <umarjihad141004@gmail.com>
2024-05-30 10:17:35 +0700

    modify server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response, Router } from 'express'
 import path from 'path'
 import userRouter from './routes/userRoute'
 
 const app: Application = express()
+const router: Router = Router()
 
 app.use(express.json(), express.static("views"))
 app.use("/styles", express.static(path.join(__dirname, "styles")))
 
-app.get("/", (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "views", "index.html"))
 })
 
 app.use("/api/users", userRouter)
+
+app.use("/", router)
 
 app.listen(8000, () => {
     console.log("Server is running")
