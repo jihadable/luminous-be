9b26495c8b04edc489d9a692175803c72694d424
jihadable <umarjihad141004@gmail.com>
2026-05-13 22:08:42 +0700

    create test file for password reset service

---
+++ test/api/passwordReset.test.ts
@@ -0,0 +1,19 @@
+describe("Password reset API", () => {
+    let customer_jwt: string
+
+    test("Send password reset email", async() => {
+
+    })
+
+    test("Send password reset with invalid email", async() => {
+
+    })
+
+    test("Reset password", async() => {
+
+    })
+
+    test("Reset password with invalid token", async() => {
+        
+    })
+})
